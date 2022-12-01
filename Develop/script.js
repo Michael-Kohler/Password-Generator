c172575eb763145aaa91e70d45135a70f3c01b10
Michael <michael.kohler2000@aol.com>
2022-11-30 19:46:11 -0500

    added random charactor generators in js

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,32 @@
-function getPassword(){
-  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$%^&*( )_+?><:{}[]'
+// const passwordEl = document.getElementById("password");
+// const lengthEl = document.getElementById("length");
+// const uppercaseEl = document.getElementById("uppercase");
+// const lowercaseEl = document.getElementById("lowercase");
+// const numbersEl = document.getElementById("numbers");
+// const symbolsEl = document.getElementById("symbols");
+// const generateEl = document.getElementById("generate");
 
-  let passwordLength = 16;
-  let password =  "";
-
-  for (let i=0; i<passwordLength; i++){
-      let randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber,randomNumber+1);
-  }
-  document.getElementById('password').value = password;
+function getRandomUpper() {
+	const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return uppercase [Math.floor(Math.random() * uppercase.length)];
 }
+
+function getRandomLower() {
+	const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  return lowercase [Math.floor(Math.random() * lowercase.length)];
+}
+
+function getRandomNumber() {
+	const numbers = '!@#$%^&*(){}[]=<>/,.';
+  return numbers [Math.floor(Math.random() * numbers.length)];
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols [Math.floor(Math.random() * symbols.length)];
+}
+
+// console.log(getRandomUpper());
+// console.log(getRandomLower());
+// console.log(getRandomNumber());
+// console.log(getRandomSymbol());
