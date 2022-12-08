fcc224415571518ed4ac2f9bb3baca8e24b60d2f
Michael <michael.kohler2000@aol.com>
2022-12-08 18:37:54 -0500

    html and css complete, js still not working

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,57 @@
-// const passwordEl = document.getElementById("password");
-// const lengthEl = document.getElementById("length");
-// const uppercaseEl = document.getElementById("uppercase");
-// const lowercaseEl = document.getElementById("lowercase");
-// const numbersEl = document.getElementById("numbers");
-// const symbolsEl = document.getElementById("symbols");
-// const generateEl = document.getElementById("generate");
+const scrollRange = document.getElementById("lengthRange");
+const scrollNum = document.getElementById("lengthNum");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const form = document.getElementById("generatorForm");
+const passwordDisplay = document.getElementById("passwordDisplay");
 
-function getRandomUpper() {
-	const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return uppercase [Math.floor(Math.random() * uppercase.length)];
+const UPPERCASE_CHAR_CODE = array(65, 90);
+const LOWERCASE_CHAR_CODE = array(97, 122);
+const NUMBER_CHAR_CODE = array(48, 57);
+const SYMBOL_CHAR_CODE = array(33, 47);
+
+scrollNum.addEventListener("input", syncCharacterAmount);
+scrollRange.addEventListener("input", syncCharacterAmount);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const length = scrollNum.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+  const password = generatePassword(
+    length,
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol
+  );
+  passwordDisplay.innerText = password;
+});
+
+function generatePassword(
+  length,
+  hasLower,
+  hasUpper,
+  hasNumber,
+  hasSymbol
+) {
+  console.log(UPPERCASE_CHAR_CODE)
 }
 
-function getRandomLower() {
-	const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-  return lowercase [Math.floor(Math.random() * lowercase.length)];
+function array(low, high) {
+  const array = []
+  for (let i = low; i < high; i++) {
+    array.push(i)
+  }
+  return array
 }
 
-function getRandomNumber() {
-	const numbers = '!@#$%^&*(){}[]=<>/,.';
-  return numbers [Math.floor(Math.random() * numbers.length)];
+function syncCharacterAmount(e) {
+  const value = e.target.value;
+  scrollNum.value = value;
+  scrollRange.value = value;
 }
-
-function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.';
-  return symbols [Math.floor(Math.random() * symbols.length)];
-}
-
-// console.log(getRandomUpper());
-// console.log(getRandomLower());
-// console.log(getRandomNumber());
-// console.log(getRandomSymbol());
